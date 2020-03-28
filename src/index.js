1c6909a5c5994a8f64f45a2b0a9694c75723141c
Kishore-p-98 <kishore.p2016@gmail.com>
2020-03-28 13:42:43 +0530

    Intialized the backend
    - Added graphqlyoga for backend
    - Using prismaClient for Db
    - Basic schema intialized
    - Added authorization with jsonwebtoken

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const { GraphQLServer } = require("graphql-yoga");
+const { prisma } = require('./generated/prisma-client/index');
+
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
+const Subscription = require('./resolvers/Subscription');
+
+const resolvers = {
+  Query,
+  Mutation,
+  User,
+  Link,
+  Subscription
+}
+
+const server = new GraphQLServer({
+  typeDefs: "./src/schema.graphql",
+  resolvers,
+  context: request => {
+    return {
+      ...request,
+      prisma,
+    }
+  },
+});
+
+server.start(() =>
+  console.log(`Server on
+ http://localhost:4000`)
+);
