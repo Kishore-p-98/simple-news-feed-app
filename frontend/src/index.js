99b11afcb7811d079dfa5df7cc82f010756437c3
Kishore-p-98 <kishore.p2016@gmail.com>
2020-04-01 01:03:44 +0530

    UI | Link,Header,CreatLink component
    - Added router for navigation
    - Added Header with route option
    - CreateLink feature added in UI

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom'
 
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
@@ -10,7 +11,10 @@ import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000'
+  uri: 'http://localhost:4000',
+  headers: {
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjazgxZTc4Y2FiaHZrMDk4MXJza2ZvOG84IiwiaWF0IjoxNTg0NzgxODU4fQ.5mEYshpGh9P5mK71BVX0kkTFTxFqUfGnmcGyeDEk1Gk'
+  }
 })
 
 const client = new ApolloClient({
@@ -19,12 +23,13 @@ const client = new ApolloClient({
 })
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 )
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
