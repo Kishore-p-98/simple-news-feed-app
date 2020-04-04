62cd1b84415087fd0d74e78bc803deaa57ce59c5
Kishore-p-98 <kishore.p2016@gmail.com>
2020-04-04 19:58:24 +0530

    Added Authentication
    - Login, Create Account features
    - Added authentication header using Appolo Cient

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,15 +10,25 @@ import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
+import { setContext } from 'apollo-link-context';
+import { AUTH_TOKEN } from './constants';
+
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000',
-  headers: {
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjazgxZTc4Y2FiaHZrMDk4MXJza2ZvOG84IiwiaWF0IjoxNTg0NzgxODU4fQ.5mEYshpGh9P5mK71BVX0kkTFTxFqUfGnmcGyeDEk1Gk'
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
   }
 })
 
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 })
 
